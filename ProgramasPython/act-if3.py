f04c31db45afc00d91b2f579d84c52d0ad012b0a
scarlet-jaraf <scarlet.jara@hotmail.com>
2024-04-17 20:03:26 -0400

    clase 17/04

---
+++ ProgramasPython/act-if3.py
@@ -0,0 +1,50 @@
+# tacos=1
+# pizza=2
+# humitas=3
+# cazuela=4
+
+# print("Elija una opccion de comida")
+# op=int(input())
+
+# if op==(1):
+#     print("Usted prefiere los tacos")
+# elif(op==(2)):
+#     print("Usted prefiere la pizza")
+# elif(op==(3)):
+#     print("Usted prefiere las humitas")    
+# elif(op==(4)):
+#     print("Usted prefiere la cazuela")
+# else:
+#     print("Usted es regodion")  
+
+#************************************************************
+
+# ticket_valido=False
+
+# folio_valido=123
+
+# print("Ingrese el numero de folio")
+# n_folio=int(input())
+
+# if n_folio==folio_valido:
+#     ticket_valido=True
+
+# if ticket_valido:
+#     print("Este ticket es válido")
+# else:
+#     print("Este ticket caducó")
+    
+    
+# edad=18
+# color="Azul"
+# # grupo="B"
+# # if edad>=18 or grupo=="A":
+# #     print("Usted es mayor y pertenece al grupo A")
+# # elif edad>=18 and grupo=="B":
+# #     print("Usted es mayor y pertenece al grupo B")
+# # else:
+# #     print("Usted no es mayor y no pertenece a ningun grupo")
+    
+# if color=="Azul" or color=="Negro" or color=="Rojo":
+#     print("Usted tiene al menos un color")
+
--- ProgramasPython/for.py
+++ ProgramasPython/for.py
@@ -1,2 +1,7 @@
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
+    
+lista_numeros = [1, 2, 3, 4, 5]
+
+for valor in lista_numeros:
+    print(valor)
