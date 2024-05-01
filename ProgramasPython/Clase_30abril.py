59ca18bacffabba7f2565b99ab73e487615ea4fa
scarlet-jaraf <scarlet.jara@hotmail.com>
2024-04-30 21:00:30 -0400

    clase 30-04

---
+++ ProgramasPython/Clase_30abril.py
@@ -0,0 +1,20 @@
+#validar si un ticket esta dentro del rango valido entre 100 y 750
+#validar si va a cancha o a galeria
+
+# ticket= int(input("Ingrese unmero de ticket: "))
+
+
+# if ticket >=100 and ticket<=750:
+#     print("Ticket valido")
+#     ubicacion= input("ingrese ubicacion cancha/galeria: ")
+#     if ubicacion=="cancha":
+#         print("Ingrese a sector cancha")
+#     elif ubicacion=="cancha":
+#         print("Ingrese a sector galeria")
+#     else:
+#         print("ubicacion no valida")
+# else:
+#     print("Ticket no valido.")
+
+
+
