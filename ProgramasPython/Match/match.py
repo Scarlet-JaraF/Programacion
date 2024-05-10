abe2b29cd1ae5fbb6c554597c3ecdd8030b95c7d
scarlet-jaraf <scarlet.jara@hotmail.com>
2024-05-09 20:02:14 -0400

    clase 9 de mayo

--- ProgramasPython/Match/match.py
+++ ProgramasPython/Match/match.py
@@ -1,4 +1,32 @@
-error = input()
 
+stay=True
+gasto=0
+while stay:
 
-#hacer un match para que usuario ingrese un numero y me mande los programas que se han visto en clases
+   print("--Bienvenido al sistema Python")
+   print("Seleccione una opcion")
+   print("1.- Escriba su nombre")
+   print("2.- Igrese su gasto")
+   print("3.- Ingrese su año de nacimiento")
+   print("4.- Ver resultados")
+   print("5.- Opcion salir")
+   opcion=int(input())
+   
+   match opcion:
+       case 1:
+           nombre=input("Escriba nombre aqui:  ")
+       case 2:
+           gasto=gasto+int(input("gasto aqui:  "))
+       case 3:
+           año=int(input("Año de nacimiento aqui: "))
+           edad=2024-año
+       case 4:
+           print("Hola ",nombre, ", su edad es ", edad, "Usted ha gastado " , gasto)
+
+           print("*************************************************************")
+       case 5:
+           print("Saliendo...")
+           stay=False
+
+       case _:
+           print("no valido")
