3f436fba346881bd4db49a5a722f1635bfdc8421
scarlet-jaraf <scarlet.jara@hotmail.com>
2024-05-16 20:16:12 -0400

    listas

---
+++ ProgramasPython/Crud.py
@@ -0,0 +1,30 @@
+
+import random
+# n=random.randint(0,100)
+# n2=random.randint(0,100)
+# lista=[1,2,3,4,5,6]
+# lista.append(n)
+# lista.append(n2)
+
+# for elemento in lista:
+#     print(elemento)
+
+
+
+bingo=[45,17,24,46,90]
+comodin=random.randint(10,99)
+
+while comodin in bingo:
+  if comodin in bingo:
+      #print("creando nuevo comodin")
+      comodin=random.randint(10,99)
+bingo.append("comodin")
+bingo.append(comodin)    
+
+
+for num in bingo:
+    print(num)
+g=random.randint(0,4)    
+print("Numero ganador ",bingo[g])    
+    
+    
