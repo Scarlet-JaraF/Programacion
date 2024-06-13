c6cac70000d0512af1961322c760d141eee7c625
scarlet-jaraf <scarlet.jara@hotmail.com>
2024-06-12 20:08:46 -0400

    funcion

--- ProgramasPython/Funciones/FuncionDiccionario.py
+++ ProgramasPython/Funciones/FuncionDiccionario.py
@@ -1,19 +1,23 @@
-
+from actividad_funciones import FuncionDiccionario
 def creaDiccionario():
     
-    dic_vacio={}
-    while True:
+      dic_vacio={}
+      while True:
          
 
-         key=input("Ingrese una key")
-         if key =="shazam":
+           key=input("Ingrese una key")
+           if key =="shazam":
               print(dic_vacio)
               break
               
-         else:
+           else:
             value=input("Ingrese el valor de key anterior")
             dic_vacio[key]=value
             print(dic_vacio)
     
+      clave=str(input("ingrese numero indice"))
+      resultado= FuncionDiccionario(dic_vacio, clave)
+
+      print(resultado)   
 
 creaDiccionario()
