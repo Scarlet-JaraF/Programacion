2ebe83b6d41a4b2510de76ec2ac9edab361fd0ff
scarlet-jaraf <scarlet.jara@hotmail.com>
2024-06-27 23:54:27 -0400

    lol

--- ProgramasPython/Archivos/estacionamientos.py
+++ ProgramasPython/Archivos/estacionamientos.py
@@ -17,6 +17,7 @@ entrada= datetime.now()
 #parking[0][0]={"patente":"ASDF11", "fecha": entrada}
 #diferencia=datetime.now() - parking[0][0]["fecha"]
 #print(diferencia)
+#print(parking[2][2]["fecha"].strftime("%H:%M"))
 
 def ingresar():
     print("Bienvenido")
@@ -45,47 +46,55 @@ while True:
             while len(patente) !=6:
                 print("Formato incorrecto, Ingrese nuevamente.")
                 patente=input().upper()
-                
+            entrada= datetime.now()
+            entra=entrada.strftime("%H:%M:%S")
             parking[piso][est]={"patente": patente, "entrada":entrada}
             est=est+1
-            #if piso==:
             if est ==29:
                 est=0
                 piso=piso+1
-            
-            print (len(parking))
-            
             print(parking)
-            print (f"cupos {len(parking)}")
+           
             print(est)
             print(piso)
             
             
         case 2:
-            patente = (input("Ingrese el vehículo a borrar: "))
-            # for n in range(0,4):
-            #     for m in range(0,29):
-            #         if borrar in parking[n][m]:
-            #             parking[n][m].remove(borrar)
-            #             print(parking)                        
-            #             print(parking[n][m]["entrada"])
-            #             #diferencia = datetime.now() - parking[n][m][1]
-            #             #print(diferencia)
-            #             registrar_salida(patente):
-            for piso in range(0,4):
-                for espacio in range(0,29):
-                    info = parking[piso][est]
-                    if info and info['patente'] == patente:
-                        hora_llegada = info['entrada']
-                        hora_salida = datetime.now()
-                        duracion = (hora_salida - hora_llegada).total_seconds() / 60
-                        costo = duracion * 15
-                        parking[piso][espacio] = None
-                        #generar_boleta(patente, hora_llegada, hora_salida, costo)
-                        print(f"Auto con patente {patente} ha salido del estacionamiento.")
-                        print (duracion, costo)
-            print("Auto no encontrado en el estacionamiento.")
-            #return None, None
+            patente = (input("Ingrese el vehículo a borrar: ")).upper()
+            #for n in range(0,4):
+            for patente in parking.items():
+               # if patente == parking[n][m]["patente"]:
+                    llego=parking[n][m]["entrada"]
+                    print(llego.strftime("%X"))
+                    salida= datetime.now()
+                    print(salida.strftime("%X"))
+                    print(f"tiempo total  {salida-llego}")
+                    
+                
+                    #parking[n][m]={}
+                    #diferencia =int(sale)-int(parking[n][m]["entrada"])
+                    #print(f"Tiempo estadia {diferencia}")                        
+                    
+                    #print(parking)                        
+            print("Auto no encontrado en el estacionamiento.")            
+                        
+                        
+
+                    
+            # for piso in range(0,4):
+            #     for espacio in range(0,29):
+            #         info = parking[piso][est]
+            #         if info and info['patente'] == patente:
+            #             hora_llegada = info['entrada']
+            #             hora_salida = datetime.now()
+            #             duracion = (hora_salida - hora_llegada).total_seconds() / 60
+            #             costo = duracion * 15
+            #             parking[piso][espacio] = None
+            #             #generar_boleta(patente, hora_llegada, hora_salida, costo)
+            #             print(f"Auto con patente {patente} ha salido del estacionamiento.")
+            #             print (duracion, costo)
+            # print("Auto no encontrado en el estacionamiento.")
+            # #return None, None
 
         
         case _:        
